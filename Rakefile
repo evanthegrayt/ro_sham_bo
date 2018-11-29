b49e00ec915c4da96673a478881ed5596da313dd
Evan Gray <egray1985@gmail.com>
2018-11-29 16:17:10 -0600

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,28 @@
+INSTALL_PATH = File.expand_path(File.join(File.dirname(__FILE__)), '..').freeze
+LINK_TO = File.join('/', 'usr', 'local', 'bin', 'rps').freeze
+LINK_FROM = File.join(INSTALL_PATH, 'bin', 'rps').freeze
+
+task :default => :install
+
+desc "Install to `/usr/local/bin`"
+task :install do
+  File.symlink(LINK_FROM, LINK_TO)
+end
+
+desc "Uninstall..."
+task :uninstall do
+  File.delete(LINK_TO) if File.symlink?(LINK_TO)
+end
+
+desc "Tagging and pulling from master"
+task :update do
+  sh("git tag #{Time.now.strftime('%Y-%m-%d-%H%M')}")
+  sh("git pull origin master")
+end
+
+desc "Checking out last deployment tag"
+task :rollback do
+  tags = `git tag`.strip.split("\n")
+  sh("git checkout #{tags.last}")
+end
+
---
+++ lib/rock_paper_scissors.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+# Simple Rock Paper Scissors game for fun.
+
+require 'optparse'
+
+class RockPaperScissors
+
+  RULES = {
+    rock:     {rock: :draw, paper: :lose, scissors: :win},
+    paper:    {rock: :win,  paper: :draw, scissors: :lose},
+    scissors: {rock: :lose, paper: :win,  scissors: :draw},
+  }.freeze
+
+  attr_reader :score, :points_to_win
+
+  def initialize(options = {rounds: 3})
+    @options = options
+    @score = {user: 0, computer: 0}
+    @points_to_win = (options[:rounds].to_f / 2).ceil
+  end
+
+  def user_choice(user_input)
+    case user_input
+    when 'r', 'rock'              then :rock
+    when 'p', 'paper'             then :paper
+    when 's', 'scissors'          then :scissors
+    when 'x', 'exit', 'q', 'quit' then :exit
+    end
+  end
+
+  def computer_choice
+    RULES.keys.sample
+  end
+
+  def round_winner(u_choice, c_choice)
+    winner =
+      case RULES[u_choice][c_choice]
+      when :win  then :user
+      when :lose then :computer
+      else :draw
+      end
+    score[winner] += 1 unless winner == :draw
+    winner
+  end
+
+  def game_winner
+    score.key(points_to_win).capitalize if score.value?(points_to_win)
+  end
+
+  def game_winner?
+    !game_winner.nil?
+  end
+
+end
+
